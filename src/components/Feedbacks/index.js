210c412c0e7f881c32e71313c90be89d4f37bead
Tema-front <polsowatel.artem@gmail.com>
2022-02-22 14:29:17 +0300

    Test task is ready

--- src/components/Feedbacks/index.js
+++ src/components/Feedbacks/index.js
@@ -5,6 +5,7 @@ export const Feedbacks = () => {
         <>
         <section className="feedbacks">
             <FeedbacksItem />
+            <div className="feedbacks-background">123</div>
         </section>
         </>
     )
