0a03a24b5ead540c7c816821260ce81c7c264a39
Tema-front <polsowatel.artem@gmail.com>
2022-02-26 17:39:16 +0300

    Modification

--- src/components/Innovations/index.js
+++ src/components/Innovations/index.js
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react"
 import { InnovationsItem } from "../InnovationsItem"
-import innovation1 from '../../img/desktop/img1.png'
-import innovation2 from '../../img/desktop/img2.png'
-import innovation3 from '../../img/desktop/img3.png'
+import desktopInnovation1 from '../../img/desktop/img1.png'
+import desktopInnovation2 from '../../img/desktop/img2.png'
+import desktopInnovation3 from '../../img/desktop/img3.png'
+
+import mobileInnovation1 from '../../img/mobile/img1.png'
+import mobileInnovation2 from '../../img/mobile/img2.png'
+import mobileInnovation3 from '../../img/mobile/post3.png'
 
 export const Innovations = ({ moreInnovation }) => {
     // Массив данных ссылок до сортирования по дате
     const [innovationsList, setInnovationsList] = useState([
         // Первоначальные значения ссылок. 
         // Указал им большую дату, чтобы при сортировке они не поменяли своего порядка в массиве и соответственно расположения на странице
-        {title: 'Instagram FAQ — All You Need To Know', url: 'https://blog.combin.com/instagram-faq-all-you-need-to-know-bb2559ac606b', img: innovation1, date: '9999-09-20'},
-        {title: 'DMEXCO 2019 — Meet Combin in Cologne', url: 'https://blog.combin.com/dmexco-2019-meet-combin-in-cologne-b845529a1e63', img: innovation2, date: '9998-09-20'},
-        {title: 'Limits Display, New User Preview, New Filters, and Many More Features — All about Combin 2.1', url: 'https://blog.combin.com/limits-display-new-user-preview-new-filters-and-many-more-features-all-about-combin-2-1-d78713383da7', img: innovation3, date: '9997-09-20'},
+        {title: 'Instagram FAQ — All You Need To Know', url: 'https://blog.combin.com/instagram-faq-all-you-need-to-know-bb2559ac606b', img: desktopInnovation1, imgMobile: mobileInnovation1, date: '9999-09-20'},
+        {title: 'DMEXCO 2019 — Meet Combin in Cologne', url: 'https://blog.combin.com/dmexco-2019-meet-combin-in-cologne-b845529a1e63', img: desktopInnovation2, imgMobile: mobileInnovation2, date: '9998-09-20'},
+        {title: 'Limits Display, New User Preview, New Filters, and Many More Features — All about Combin 2.1', url: 'https://blog.combin.com/limits-display-new-user-preview-new-filters-and-many-more-features-all-about-combin-2-1-d78713383da7', img: desktopInnovation3, imgMobile: mobileInnovation3, date: '9997-09-20'},
     ]);
     // Сюда будет попадать отсортированные по дате данные ссылок
     const [sortInnovation, setSortInnovation] = useState([])
@@ -21,7 +25,7 @@ export const Innovations = ({ moreInnovation }) => {
         if (moreInnovation) {
             for (let i = 0; i < moreInnovation.length; i++) {
                 setInnovationsList(prev => {
-                    return [...prev, {...moreInnovation[i], img: [innovation1, innovation2, innovation3][i % 3]}]
+                    return [...prev, {...moreInnovation[i], img: [desktopInnovation1, desktopInnovation2, desktopInnovation3][i % 3], imgMobile: [mobileInnovation1, mobileInnovation2, mobileInnovation3][i % 3]}]
                 })
             }
         }
--- src/components/InnovationsItem/index.js
+++ src/components/InnovationsItem/index.js
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react"
 
 
 export const InnovationsItem = ({ innovation }) => {
+    const [mobile, setMobile] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("resize", checkWidth)
+        
+        return (() => {
+            window.removeEventListener("resize", checkWidth)
+        })
+    }, [])
+
+    const checkWidth = () => {
+        const widthPage = document.documentElement.scrollWidth;
+        if (widthPage <= 320) {
+            setMobile(true)
+        } else setMobile(false)
+    }
+
     return (
         <>
         <section className="innovations-item-wrp">
             <a className="innovations-item" href={innovation.url} target='_blank' rel="noreferrer" >
-                <img className="innovations-item-preview" src={innovation.img} alt="innovation-preview" height="486" />
+                {mobile ?    
+                    <img className="innovations-item-preview" src={innovation.imgMobile} alt="innovation-preview" height="310" />
+                    :
+                    <img className="innovations-item-preview" src={innovation.img} alt="innovation-preview" height="486" />
+                }
                 <h3 className="innovations-item-title">{innovation.title}</h3>
             </a>
         </section>
